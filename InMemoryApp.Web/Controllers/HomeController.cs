66a06c3abf5b2b4be6b474fde2cf08be4331cd17
Mesut yiğit <yigit.mesut@outlook.com>
2026-07-18 13:59:19 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
