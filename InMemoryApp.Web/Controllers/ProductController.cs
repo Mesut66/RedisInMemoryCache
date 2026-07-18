c973ac768874a3d5376b2a49ddccc16b44e31d65
Mesut yiğit <yigit.mesut@outlook.com>
2026-07-18 16:10:34 +0300

    Cache ve silinme sebebi yazdırma

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 
 namespace InMemoryApp.Web.Controllers
 {
@@ -13,34 +14,74 @@ namespace InMemoryApp.Web.Controllers
         }
         public IActionResult Index()
         {
-            //1.yol
-            if (string.IsNullOrEmpty(_memoryCache.Get<string>("ProductName")))
+
+            if (_memoryCache.TryGetValue("callback", out string callbackMessage))
             {
-                //Get ve Set üzerinden memoriye bir data kaydetme ve okuma işlemi yapabiliriz.
-                _memoryCache.Set("ProductName", "Laptop");
-                var productName = _memoryCache.Get<string>("ProductName");
-                ViewBag.ProductName = productName;
+                ViewBag.CallbackMessage = callbackMessage;
             }
 
-            //2.yol   out: bir methodda birden fazla değer dönebilmek için kullanılır.
-            if (!_memoryCache.TryGetValue("ProductName", out string productName2))
+            //"ProductName" cache'de yoksa ilk kez oluştur
+            if (!_memoryCache.TryGetValue("ProductName", out string productName))
             {
-                _memoryCache.Set("ProductName", "Laptop");
-                productName2 = _memoryCache.Get<string>("ProductName");
-                ViewBag.ProductName2 = productName2;
+                productName = "Laptop";
+
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+                options.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
+
+                // Veri silindiğinde çalışacak metot
+                options.RegisterPostEvictionCallback((key, value, reason, state) =>
+                {
+                    // Burası süre dolduğunda arka planda çalışır
+                    _memoryCache.Set("callback", $"{key} anahtarlı veri ({value}) bellekten silindi. Sebep: {reason}");
+                });
+
+                _memoryCache.Set("ProductName", productName, options);
             }
 
-            //cache de bişey alamazsa oluşturur
-            _memoryCache.GetOrCreate<string>("ProductName", entry =>
-            {
-                //cache de 10 sn tutacak
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
-                return "Macbook";
-            });
-
+            ViewBag.ProductName = productName;
+            ViewBag.CallbackMessage = callbackMessage;
 
             return View();
+
+
+            //2.yol   out: bir methodda birden fazla değer dönebilmek için kullanılır.
+            //if (string.IsNullOrEmpty(_memoryCache.Get<string>("ProductName")))
+            //{
+
+            //    MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            //    options.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30); //cache de 30 sn tutacak
+
+            //    options.SlidingExpiration = TimeSpan.FromSeconds(10); //cache de 10 sn boyunca kullanılmazsa silinecek
+
+            //    //options.Priority = CacheItemPriority.High;//High: bu data önemli silme low:memory dolarsa sil neverRomeve: memory dolsada silme
+
+
+            //    //Bir datanın hangi sebepten dolayı memory den silindiğini söyler
+            //    options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            //    {
+            //        _memoryCache.Set("callback", $"{key} => {value} => Sebep: {reason}");
+            //    });
+
+
+            //_memoryCache.TryGetValue("ProductName", out string productName3);
+
+            //productName3 = _memoryCache.Get<string>("ProductName");
+            //ViewBag.productName3 = productName3;
+
+
+        }
+
+
+            //cache de bişey alamazsa oluşturur
+            //_memoryCache.GetOrCreate<string>("ProductName", entry =>
+            //{
+            //    //cache de 10 sn tutacak
+            //    entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
+            //    return "Macbook";
+            //});
         }
 
     }
-}
+
