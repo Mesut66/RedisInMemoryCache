8d285064003c2659e99928b6107341fb2f1ffd52
Mesut yiğit <yigit.mesut@outlook.com>
2026-09-19 14:32:26 +0300

    Redia Memory den veri okuma

---
+++ RedisAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDatabase(0);
+        }
+        public IActionResult Index()
+        {
+            db.StringSet("mykey", "Hello, Redis!");
+            db.StringSet("mykey2", "Hello, Redis2!");
+            return View();
+        }
+
+        public IActionResult GetValue()
+        {
+            var value = db.StringGet("mykey");
+            var value2 = db.StringGet("mykey2");
+            ViewBag.Value = value;
+            ViewBag.Value2 = value2;
+            return View();
+        }
+    }
+}
