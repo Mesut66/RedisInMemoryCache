f29e2396dbbf2d01eb031220880aa566d9a1fe3e
Mesut yiğit <yigit.mesut@outlook.com>
2026-09-19 14:04:41 +0300

    RediAPI Proje

---
+++ RedisAPI.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using RedisAPI.Web.Models;
+using System.Diagnostics;
+
+namespace RedisAPI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RedisAPI.Web/Services/RedisService.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+
+namespace RedisAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+
+        private ConnectionMultiplexer _redis;
+        public IDatabase db { get; private set; }
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(configString);
+
+        }
+
+        //redis db ye yazacak ama bende kurulmadu: Redis 15 d var hangisine söylersen yazar
+        public IDatabase GetDatabase(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
